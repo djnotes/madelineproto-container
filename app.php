7001ee7c92e572e6c7d6de8799335447c5967561
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-02-20 10:32:17 -0700

    Removed entrypoint for simplicity. Updated README.

---
+++ app.php
@@ -0,0 +1,26 @@
+<?php
+     
+     include __DIR__ . '/vendor/autoload.php';
+
+     use \danog\MadelineProto\API;
+
+     $username = readline("Welcome to MadelineProto Container \n
+     Please enter a Telegram username (preferrably yours):");
+
+
+     $api = new API('session/session.madeline');
+
+    
+          
+     $api->async(true);
+     
+     $api->loop(function() use ($api, $username){
+
+         yield $api->start();
+         
+         try{
+             yield $api->messages->sendMessage(['peer' => $username, 'message' => 'Hello from MadelineProto Container 😎']);
+         }catch(\danog\MadelineProto\Exception $e){
+             echo "{$e->getMessage()}\n";
+         }
+     }); 
